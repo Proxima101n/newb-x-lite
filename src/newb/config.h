1017b8950d9b7b31ad1a36779775c74c9ddd75b6
Proxima101n <205085281+Proxima101n@users.noreply.github.com>
2026-03-25 11:39:15 +0530

    Update config.h
    im highkey dumb

--- src/newb/config.h
+++ src/newb/config.h
@@ -177,7 +177,7 @@
 #define NL_SHOOTING_STAR_DELAY 64.0 // 0.0 no delay ~ 100.0 long time (100 secs)
 
 /* Galaxy */
-#define NL_GALAXY_STARS 0.0         // [toggle] 0.1 dim - 4.0 bright
+//#define NL_GALAXY_STARS 0.0         // [toggle] 0.1 dim - 4.0 bright
 #define NL_GALAXY_VIBRANCE 0.0          // 0.0 white - 1.0 colorful
 #define NL_GALAXY_SPEED 0.001            // 0.01 slow motion - 0.2 fast motion
 #define NL_GALAXY_DAY_VISIBILITY 0.0    // 0.0 invisible - 1.0 visible
